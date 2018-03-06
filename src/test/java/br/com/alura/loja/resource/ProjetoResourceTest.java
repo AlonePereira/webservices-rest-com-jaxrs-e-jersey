7c90c5bbfcf368c9a0cb6d710e21cb5cf5ab94ab
Alone Silva <alone.silva@reservafacil.tur.br>
2018-03-06 07:41:17 -0300

    Criando recursos e o POST

--- src/test/java/br/com/alura/loja/resource/ProjetoResourceTest.java
+++ src/test/java/br/com/alura/loja/resource/ProjetoResourceTest.java
@@ -2,7 +2,10 @@ package br.com.alura.loja.resource;
 
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 import org.glassfish.grizzly.http.server.HttpServer;
 import org.junit.After;
@@ -38,6 +41,20 @@ public class ProjetoResourceTest {
 	Projeto projeto = (Projeto) new XStream().fromXML(conteudo);
 	
 	Assert.assertEquals("Minha loja", projeto.getNome());
+    }
+    
+    @Test
+    public void testaAdicionarProjeto() {
+	Client client = ClientBuilder.newClient();
+	WebTarget target = client.target("http://localhost:8080");
+	
+	Projeto projeto = new Projeto(3l, "PlasUtil", 2017);
+	String xml = projeto.toXML();
+	
+	Entity<String> entity = Entity.entity(xml, MediaType.APPLICATION_XML);
+	Response response = target.path("/projetos").request().post(entity);
+	
+	Assert.assertEquals("<status>sucesso</status>", response.readEntity(String.class));
     }
     
 }
